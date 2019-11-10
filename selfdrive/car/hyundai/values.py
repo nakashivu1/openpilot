5e18336f281b97f3185e600e8d3301a83a153235
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2019-11-09 20:05:35 -0500

    increase delta up

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -5,7 +5,7 @@ from selfdrive.car import dbc_dict
 class SteerLimitParams:
   STEER_MAX = 408   # 409 is the max, 255 is stock
   STEER_DELTA_UP = 4
-  STEER_DELTA_DOWN = 6
+  STEER_DELTA_DOWN = 7
   STEER_DRIVER_ALLOWANCE = 50
   STEER_DRIVER_MULTIPLIER = 2
   STEER_DRIVER_FACTOR = 1
