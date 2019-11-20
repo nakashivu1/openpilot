4bcc3bd1ea3875684e4686c915bb9d0cebec81e2
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2019-11-19 22:53:50 -0500

    revert

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -100,8 +100,9 @@ CHECKSUM = {
 }
 
 FEATURES = {
-  "use_cluster_gears": [CAR.KONA],         # Use Cluster for Gear Selection, rather than Transmission
-  "use_tcu_gears": [CAR.KIA_OPTIMA, CAR.ELANTRA, CAR.KIA_OPTIMA_1],     # Use TCU Message for Gear Selection
+  "use_cluster_gears": [CAR.KONA, CAR.ELANTRA_GT_I30],     # Use Cluster for Gear Selection, rather than Transmission
+  "use_tcu_gears": [CAR.ELANTRA, CAR.KIA_OPTIMA],                                    # Use TCU Message for Gear Selection
+  "use_new_gears": [CAR.KIA_OPTIMA_H],
   "non_scc": [CAR.IONIQ, CAR.ELANTRA, CAR.KONA, CAR.KIA_FORTE, CAR.SANTA_FE_1],      # Car without SCC
 }
 
