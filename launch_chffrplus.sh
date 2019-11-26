81e90a2ffcc3effb9a0c6f2bd95a3e063397b15d
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2019-11-25 23:27:04 -0500

    touch all file before checkout

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -18,7 +18,7 @@ function launch {
 
     # Touch all files on release2 after checkout to prevent rebuild
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
-    if [[ "$BRANCH" == "mad6.4_elantratune" ]]; then
+    if [[ "$BRANCH" == "MDPS6.4_elantra" ]]; then
         touch **
     fi
 
