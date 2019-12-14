9c23dbaa6825309eb5469ad8b91a0411273f513d
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2019-12-13 23:37:12 -0500

    tune

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -17,7 +17,7 @@ class SteerLimitParams:
   STEER_DRIVER_FACTOR = 1
 
 class LowSpeedSteerLimitParams(SteerLimitParams):
-  STEER_MAX = 300
+  STEER_MAX = 250
   STEER_DELTA_UP = 2
   STEER_DELTA_DOWN = 4
   STEER_DRIVER_ALLOWANCE = 50
@@ -73,7 +73,7 @@ class CarController():
   def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd, visual_alert,
               left_line, right_line, left_lane_depart, right_lane_depart):
 
-    if CS.left_blinker_flash or CS.right_blinker_flash or abs(CS.angle_steers) > 95.:
+    if CS.left_blinker_flash or CS.right_blinker_flash or abs(CS.angle_steers) > 90.:
       self.turning_signal_timer = 100  # Disable for 1 Seconds 
     if self.turning_signal_timer:
       enabled = 0
@@ -81,7 +81,7 @@ class CarController():
     ### Steering Torque
     apply_steer = actuators.steer * SteerLimitParams.STEER_MAX
 
-    if CS.v_ego < 6:
+    if CS.v_ego < 7:
       apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.steer_torque_driver, LowSpeedSteerLimitParams)
     else:
       apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.steer_torque_driver, SteerLimitParams)
