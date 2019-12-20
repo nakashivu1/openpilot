318ddcbfc0597d3a41fe493cab4c41200d80aa3d
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2019-12-20 17:58:27 -0500

    allow CLU11 to be sent to MDPS

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -6,20 +6,22 @@ const int HYUNDAI_MAX_RATE_DOWN = 7;
 const int HYUNDAI_DRIVER_TORQUE_ALLOWANCE = 50;
 const int HYUNDAI_DRIVER_TORQUE_FACTOR = 2;
 
-const AddrBus HYUNDAI_TX_MSGS[] = {{832, 0}, {1265, 0}};
+const AddrBus HYUNDAI_TX_MSGS[] = {{832, 0}, {832, 1}, {1265, 1}, {1265, 2}, {1057, 0}};
 
 int hyundai_rt_torque_last = 0;
 int hyundai_desired_torque_last = 0;
 int hyundai_cruise_engaged_last = 0;
 uint32_t hyundai_ts_last = 0;
 struct sample_t hyundai_torque_driver;         // last few driver torques measured
+bool hyundai_has_scc = 0;
+int OP_LKAS_live = 0;
 
 static void hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   int bus = GET_BUS(to_push);
   int addr = GET_ADDR(to_push);
 
-  if (addr == 897) {
-    int torque_driver_new = ((GET_BYTES_04(to_push) >> 11) & 0xfff) - 2048;
+  if (addr == 593) {
+    int torque_driver_new = (GET_BYTES_04(to_push) & 0x7ff) - 1024;
     // update array of samples
     update_sample(&hyundai_torque_driver, torque_driver_new);
   }
@@ -31,6 +33,7 @@ static void hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
   // enter controls on rising edge of ACC, exit controls on ACC off
   if (addr == 1057) {
+    hyundai_has_scc = 1;
     // 2 bits: 13-14
     int cruise_engaged = (GET_BYTES_04(to_push) >> 13) & 0x3;
     if (cruise_engaged && !hyundai_cruise_engaged_last) {
@@ -41,6 +44,19 @@ static void hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     }
     hyundai_cruise_engaged_last = cruise_engaged;
   }
+  // cruise control for car without SCC
+  if ((addr == 871) && (!hyundai_has_scc)) {
+    // first byte
+    int cruise_engaged = (GET_BYTES_04(to_push) & 0xFF);
+    if (cruise_engaged && !hyundai_cruise_engaged_last) {
+      controls_allowed = 1;
+    }
+    if (!cruise_engaged) {
+      controls_allowed = 0;
+    }
+    hyundai_cruise_engaged_last = cruise_engaged;
+  }
+
 }
 
 static int hyundai_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
@@ -62,6 +78,8 @@ static int hyundai_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
     int desired_torque = ((GET_BYTES_04(to_send) >> 16) & 0x7ff) - 1024;
     uint32_t ts = TIM2->CNT;
     bool violation = 0;
+	
+	OP_LKAS_live = 20;
 
     if (controls_allowed) {
 
@@ -107,7 +125,7 @@ static int hyundai_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   // FORCE CANCEL: safety check only relevant when spamming the cancel button.
   // ensuring that only the cancel button press is sent (VAL 4) when controls are off.
   // This avoids unintended engagements while still allowing resume spam
-  if ((addr == 1265) && !controls_allowed) {
+  if (((addr == 1265) && bus == 0) && !controls_allowed) {
     if ((GET_BYTES_04(to_send) & 0x7) != 4) {
       tx = 0;
     }
@@ -124,9 +142,34 @@ static int hyundai_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   // forward cam to ccan and viceversa, except lkas cmd
   if (!relay_malfunction) {
     if (bus_num == 0) {
-      bus_fwd = 2;
+      // EON create CLU12 for MDPS
+      if ((!OP_LKAS_live) || (addr != 1265)) {
+        bus_fwd = 12;
+      } else {
+        bus_fwd = 2;
+      }
     }
-    if ((bus_num == 2) && (addr != 832)) {
+    if (bus_num == 1) {
+      bus_fwd = 20;
+    }
+    if (bus_num == 2) {
+      if (addr != 832) {
+        if ((addr != 1057) || (!OP_LKAS_live)) {
+          bus_fwd = 10;
+        } else {
+          bus_fwd = 1;
+        }
+      } else if (!OP_LKAS_live) {
+        bus_fwd = 10;
+      } else {
+        OP_LKAS_live -= 1;
+      }
+    }
+  } else {
+    if (bus_num == 0) {
+      bus_fwd = 1;
+    }
+    if (bus_num == 1) {
       bus_fwd = 0;
     }
   }
