4ab68ad128dda1f1c5a152cde383398211bbea5c
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2019-12-19 22:07:04 -0500

    mdps speed spoofing

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -2,7 +2,7 @@ from cereal import car
 from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.hyundai.hyundaican import create_lkas11, create_lkas12, \
                                              create_1191, create_1156, \
-                                             create_clu11
+                                             create_clu11, create_mdps12
 from selfdrive.car.hyundai.values import Buttons
 from selfdrive.can.packer import CANPacker
 
@@ -90,6 +90,7 @@ class CarController():
     can_sends = []
 
     self.lkas11_cnt = frame % 0x10
+    self.mdps12_cnt = frame % 0x100
 
     if self.camera_disconnected:
       if (frame % 10) == 0:
@@ -98,11 +99,18 @@ class CarController():
         can_sends.append(create_1191())
       if (frame % 7) == 0:
         can_sends.append(create_1156())
+    elif not pcm_cancel_cmd:
+      can_sends.append(create_mdps12(self.packer, self.car_fingerprint, self.mdps12_cnt, CS.mdps12))
 
-    can_sends.append(create_lkas11(self.packer, self.car_fingerprint, apply_steer, steer_req, self.lkas11_cnt,
+    can_sends.append(create_lkas11(self.packer, self.car_fingerprint, 0, apply_steer, steer_req, self.lkas11_cnt,
                                    enabled, CS.lkas11, hud_alert, lane_visible, left_lane_depart, right_lane_depart,
                                    keep_stock=(not self.camera_disconnected)))
-
+    can_sends.append(create_lkas11(self.packer, self.car_fingerprint, 1, apply_steer, steer_req, self.lkas11_cnt,
+                                   enabled, CS.lkas11, hud_alert, lane_visible, left_lane_depart, right_lane_depart,
+                                   keep_stock=(not self.camera_disconnected)))
+    low_speed = 56 if CS.v_ego < 16 else 0
+    can_sends.append(create_clu11(self.packer, CS.clu11, Buttons.NONE, low_speed, self.lkas11_cnt))
+    
     #if pcm_cancel_cmd:
       #self.clu11_cnt = frame % 0x10
       #can_sends.append(create_clu11(self.packer, CS.clu11, Buttons.CANCEL, self.clu11_cnt))
@@ -115,7 +123,7 @@ class CarController():
         self.clu11_cnt = 0
       # when lead car starts moving, create 6 RES msgs
       elif CS.lead_distance > self.last_lead_distance and (frame - self.last_resume_frame) > 5:
-        can_sends.append(create_clu11(self.packer, CS.clu11, Buttons.RES_ACCEL, self.clu11_cnt))
+        can_sends.append(create_clu11(self.packer, CS.clu11, Buttons.RES_ACCEL, 0, self.clu11_cnt))
         self.clu11_cnt += 1
         # interval after 6 msgs
         if self.clu11_cnt > 5:
