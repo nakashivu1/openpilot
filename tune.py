1dce3bfa1057932f6e9bdb6dacc537842508bfac
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2019-12-24 01:46:06 -0500

    remove unnecessary tune values

--- tune.py
+++ tune.py
@@ -41,11 +41,8 @@ button_delay = 0.2
 kegman = kegman_conf()
 kegman.conf['tuneGernby'] = "1"
 #kegman.write_config(kegman.conf)
-param = ["Kp", "Ki", "Kf", "steerRatio", "sR_boost", "sR_BP0", \
-         "sR_BP1", "sR_time", "steerRateCost", "deadzone", "slowOnCurves", \
-         "1barBP0", "1barBP1", "1barMax", "2barBP0", "2barBP1", \
-         "2barMax", "3barBP0", "3barBP1", "3barMax", \
-         "1barHwy", "2barHwy", "3barHwy"]
+param = ["Kp", "Ki", "Kf", "steerRatio", "steerRateCost", "deadzone", \
+         "sR_boost", "sR_BP0", "sR_BP1", "sR_time"]
 
 j = 0
 while True:
