ecc2670a79cfb26e93547a45ceb925a4a99ba329
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2020-02-05 22:35:15 -0500

    bug

--- panda/board/main.c
+++ panda/board/main.c
@@ -705,15 +705,15 @@ void TIM1_BRK_TIM9_IRQ_Handler(void) {
       if (current_safety_mode != SAFETY_ALLOUTPUT) {
         set_safety_mode(SAFETY_ALLOUTPUT, 0U); // MDPS will hard fault if SAFETY_NOOUTPUT
       }
-	 // MDPS will fault if panda sleep
-     // if (power_save_status != POWER_SAVE_STATUS_ENABLED) {
-     //   set_power_save_state(POWER_SAVE_STATUS_ENABLED);
-     // }
-    }
 
       // Also disable fan and IR when the heartbeat goes missing
       current_board->set_fan_power(0U);
       current_board->set_ir_power(0U);
+
+	 // MDPS will fault if panda sleep
+     // if (power_save_status != POWER_SAVE_STATUS_ENABLED) {
+     //   set_power_save_state(POWER_SAVE_STATUS_ENABLED);
+     // }
     }
 
     // enter CDP mode when car starts to ensure we are charging a turned off EON
