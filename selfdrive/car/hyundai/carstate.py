1305260c4f09c596a5501a502a478b66788da628
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2020-02-05 21:58:12 -0500

    add blinker flash

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -319,6 +319,8 @@ class CarState():
     # update prevs, update must run once per Loop
     self.prev_left_blinker_on = self.left_blinker_on
     self.prev_right_blinker_on = self.right_blinker_on
+    self.prev_left_blinker_flash = self.left_blinker_flash
+    self.prev_right_blinker_flash = self.right_blinker_flash
 
     self.door_all_closed = True
     self.seatbelt = cp.vl["CGW1"]['CF_Gway_DrvSeatBeltSw']
@@ -360,9 +362,20 @@ class CarState():
     self.angle_steers_rate = cp_sas.vl["SAS11"]['SAS_Speed']
     self.yaw_rate = cp.vl["ESP12"]['YAW_RATE']
     self.left_blinker_on = cp.vl["CGW1"]['CF_Gway_TSigLHSw']
-    self.left_blinker_flash = cp.vl["CGW1"]['CF_Gway_TurnSigLh']
     self.right_blinker_on = cp.vl["CGW1"]['CF_Gway_TSigRHSw']
-    self.right_blinker_flash = cp.vl["CGW1"]['CF_Gway_TurnSigRh']
+
+    # make blinker flash to be continuous
+    if self.v_ego > 17.5:
+      self.left_blinker_cnt = 400 if cp.vl["CGW1"]['CF_Gway_TurnSigLh'] else max(self.left_blinker_cnt - 1, 0)
+      self.left_blinker_flash = self.left_blinker_cnt > 0
+    else:
+      self.left_blinker_flash = cp.vl["CGW1"]['CF_Gway_TurnSigLh']
+    if self.v_ego > 17.5:
+      self.right_blinker_cnt = 400 if cp.vl["CGW1"]['CF_Gway_TurnSigRh'] else max(self.right_blinker_cnt - 1, 0)
+      self.right_blinker_flash = self.right_blinker_cnt > 0
+    else:
+      self.right_blinker_flash = cp.vl["CGW1"]['CF_Gway_TurnSigRh']
+
     self.steer_override = abs(cp_mdps.vl["MDPS12"]['CR_Mdps_StrColTq']) > STEER_THRESHOLD
     self.steer_state = cp_mdps.vl["MDPS12"]['CF_Mdps_ToiActive'] #0 NOT ACTIVE, 1 ACTIVE
     self.steer_error = cp_mdps.vl["MDPS12"]['CF_Mdps_ToiUnavail']
