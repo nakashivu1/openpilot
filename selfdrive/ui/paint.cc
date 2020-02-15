b703f12b032a8e31f2a1a8b424e2370b69130c92
clarity-bru <clarity.bru@gmail.com>
2020-02-14 19:00:05 -0800

    added bat lvl

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -868,7 +868,37 @@ static void bb_ui_draw_measures_left(UIState *s, int bb_x, int bb_y, int bb_w ) 
     bb_ry = bb_y + bb_h;
   }
   */
+    //Read the file with the battery level.  Not expecting anything above 100%
+    fd = open("/sys/class/power_supply/battery/capacity", O_RDONLY);
+    if(fd == -1)
+    {
+      //can't open
+    }
+    else
+    {
+      read(fd, &bat_lvl, 3);
+    }
 
+    //clean up the last char (wierd rectangle symbol) in the line
+    for (int i=1; i<4; i++)
+    {
+      //if char is not a digit then replace it with null
+      if(isdigit(bat_lvl[i]) == 0)
+          {
+            bat_lvl[i] = '\0';
+            break;
+          }
+    }
+    close(fd);
+
+    snprintf(val_str, sizeof(val_str), "%s%%", bat_lvl);
+    snprintf(uom_str, sizeof(uom_str), "");
+    bb_h +=bb_ui_draw_measure(s,  val_str, uom_str, "BAT LVL",
+        bb_rx, bb_ry, bb_uom_dx,
+        val_color, lab_color, uom_color,
+        value_fontSize, label_fontSize, uom_fontSize );
+    bb_ry = bb_y + bb_h;
+  }
   //add grey panda GPS accuracy
   /*if (true) {
     char val_str[16];
