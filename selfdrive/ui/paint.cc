c7f8da0bc064cdcf76f2a2e176e995eccd2e12ce
clarity-bru <clarity.bru@gmail.com>
2020-02-14 21:36:56 -0800

    battery temp indicator

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -4,6 +4,8 @@
 #include <time.h>//clarity-bru: time
 #include <string.h>//clarity-bru: strcpy
 #include <unistd.h>//clarity-bru: files
+#include <stdio.h>//clarity-bru: files
+
 #include "ui.hpp"
 
 #include "common/util.h"
