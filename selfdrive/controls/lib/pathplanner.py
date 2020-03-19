c1293506a7d68ee77c3347da4fda840af39aadba
nakashivu1 <55726583+nakashivu1@users.noreply.github.com>
2020-03-19 15:15:08 -0400

    some tweak

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -40,8 +40,7 @@ DESIRES = {
 
 def calc_states_after_delay(states, v_ego, steer_angle, curvature_factor, steer_ratio, delay):
   states[0].x = v_ego * delay
-  states[0].delta = math.radians(steer_angle) / steer_ratio
-  states[0].psi = curvature_factor * states[0].delta * states[0].x
+  states[0].psi = v_ego * curvature_factor * math.radians(steer_angle) / steer_ratio * delay
   return states
 
 
@@ -213,6 +212,9 @@ class PathPlanner():
     # account for actuation delay
     self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset, curvature_factor, VM.sR, CP.steerActuatorDelay)
 
+    if abs(angle_steers - angle_offset) > 4 and CP.lateralTuning.which() == 'pid': 
+      self.cur_state[0].delta = math.radians(angle_steers - angle_offset) / VM.sR
+
     v_ego_mpc = max(v_ego, 5.0)  # avoid mpc roughness due to low speed
     self.libmpc.run_mpc(self.cur_state, self.mpc_solution,
                         list(self.LP.l_poly), list(self.LP.r_poly), list(self.LP.d_poly),
@@ -225,6 +227,8 @@ class PathPlanner():
     else:
       delta_desired = math.radians(angle_steers - angle_offset) / VM.sR
       rate_desired = 0.0
+
+    self.cur_state[0].delta = delta_desired
 
     self.angle_steers_des_mpc = float(math.degrees(delta_desired * VM.sR) + angle_offset)
 
