0dfb2edfd7d02bff89f3eaeeb3c52578c1ace43b
Nakashi <nakashivu1@icloud.com>
2020-06-15 06:20:17 -0400

    not needed

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -90,7 +90,6 @@ FEATURES = {
   "use_cluster_gears": [CAR.KONA, CAR.ELANTRA_GT_I30],     # Use Cluster for Gear Selection, rather than Transmission
   "use_tcu_gears": [CAR.ELANTRA, CAR.KIA_OPTIMA],                                    # Use TCU Message for Gear Selection
   "use_elect_gears": [CAR.KIA_OPTIMA_H, CAR.IONIQ_EV_LTD],              # Use TCU Message for Gear Selection
-  "non_scc": [CAR.IONIQ, CAR.KONA, CAR.KIA_FORTE, CAR.SANTA_FE_1, CAR.ELANTRA],      # Car without SCC
 }
 
 DBC = {
