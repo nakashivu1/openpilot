900b707195564504332b71dc76a42948635529ee
Nakashi <nakashivu1@icloud.com>
2020-06-14 22:41:25 -0400

    tune

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -266,7 +266,7 @@ class PathPlanner():
     # account for actuation delay
     self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset, curvature_factor, self.steerRatio, CP.steerActuatorDelay)
 
-    if v_ego < 9.0 or (v_ego > 27.0 and abs(angle_steers) < 10) or (abs(angle_steers - angle_offset) > 20 and v_ego < 27.0):
+    if v_ego < 5.0 or (v_ego > 35.0 and abs(angle_steers) < 4) or (abs(angle_steers - angle_offset) > 20 and v_ego < 27.0):
       self.cur_state[0].delta = math.radians(angle_steers - angle_offset) / self.steerRatio
 
     v_ego_mpc = max(v_ego, 5.0)  # avoid mpc roughness due to low speed
